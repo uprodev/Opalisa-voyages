199713a1353c6fb3e8c9d097d6f90fb2b5d0cbae
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2025-02-10 16:09:30 +0100

    Update faq.php

--- wp-theme/opalisa/templates/guttenberg/faq.php
+++ wp-theme/opalisa/templates/guttenberg/faq.php
@@ -8,56 +8,29 @@ $faqs = get_field('faqs');
 <section class="faq">
     <div class="content-width">
         <div class="content">
-            <h2>Questions fréquentes</h2>
-            <ul class="accordion">
-                <li class="accordion-item">
-                    <div class="accordion-thumb"><p>Le prix affiché est-il tout compris ?</p> <span><i class="fa-regular fa-arrow-right"></i></span></div>
-                    <div class="accordion-panel">
-                        <div class="wrap">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing
-                                elit. Placeat, quibusdam! Voluptate nobis, beatae
-                                tempora praesentium, illum quis illo, maiores quod
-                                similique placeat, saepe mollitia dolor officiis
-                                aspernatur deleniti debitis commodi!</p>
-                        </div>
-                    </div>
-                </li>
-                <li class="accordion-item ">
-                    <div class="accordion-thumb"><p>Faites-vous uniquement les billets d’avion ?</p> <span><i class="fa-regular fa-arrow-right"></i></span></div>
-                    <div class="accordion-panel">
-                        <div class="wrap">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing
-                                elit. Placeat, quibusdam! Voluptate nobis, beatae
-                                tempora praesentium, illum quis illo, maiores quod
-                                similique placeat, saepe mollitia dolor </p>
-                        </div>
-                    </div>
-                </li>
-                <li class="accordion-item ">
-                    <div class="accordion-thumb"><p>Est-il possible de modifier mon billet après réservation ?</p> <span><i class="fa-regular fa-arrow-right"></i></span></div>
-                    <div class="accordion-panel">
-                        <div class="wrap">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing
-                                elit. Placeat, quibusdam! Voluptate nobis, beatae
-                                tempora praesentium, illum quis illo, maiores quod
-                                similique placeat, saepe mollitia dolor officiis
-                                aspernatur deleniti debitis commodi!</p>
-                        </div>
-                    </div>
-                </li>
-                <li class="accordion-item ">
-                    <div class="accordion-thumb"><p>Le service client est-il joignable en cas d’inquiétude ?</p> <span><i class="fa-regular fa-arrow-right"></i></span></div>
-                    <div class="accordion-panel">
-                        <div class="wrap">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing
-                                elit. Placeat, quibusdam! Voluptate nobis, beatae
-                                tempora praesentium, illum quis illo, maiores quod
-                                similique placeat, saepe mollitia dolor officiis
-                                aspernatur deleniti debitis commodi!</p>
-                        </div>
-                    </div>
-                </li>
-            </ul>
+
+            <?php if($title){ ?>
+                <h2><?= $title;?></h2>
+            <?php } ?>
+
+            <?php if($faqs): ?>
+                <ul class="accordion">
+                    <?php foreach($faqs as $faq):
+                        $faq_post = get_post($faq);
+                        ?>
+                        <li class="accordion-item">
+                            <div class="accordion-thumb">
+                                <p><?= get_the_title($faq);?></p><span><i class="fa-regular fa-arrow-right"></i></span>
+                            </div>
+                            <div class="accordion-panel">
+                                <div class="wrap">
+                                    <?= apply_filters('the_content', $faq_post->post_content);;?>
+                                </div>
+                            </div>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif;?>
         </div>
     </div>
 </section>

e62bf15409ada2b5854a2952367a3976b22e5a1e
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2025-02-10 15:15:07 +0100

    Update offers.php

--- wp-theme/opalisa/templates/guttenberg/offers.php
+++ wp-theme/opalisa/templates/guttenberg/offers.php
@@ -1,6 +1,9 @@
 <?php
 
-
+$link = get_field('link');
+$text = get_field('text');
+$image = get_field('image');
+$offers = get_field('offers');
 
 ?>
 
@@ -8,11 +11,18 @@
     <div class="content-width">
         <div class="title">
             <div class="left">
-                <h2>Découvrez les meilleurs offres </h2>
-                <p>Le bonheur n’est pas une destination à atteindre : c’est une manière de voyager</p>
+                <?php if($text){
+                    echo $text;
+                } ?>
             </div>
             <div class="right">
-                <a href="#">Découvrir toutes les offres <i class="fa-regular fa-arrow-right"></i></a>
+                <?php if( $link ):
+                    $link_url = $link['url'];
+                    $link_title = $link['title'];
+                    $link_target = $link['target'] ? $link['target'] : '_self';
+                    ?>
+                    <a class="" href="<?= esc_url($link_url); ?>" target="<?= esc_attr($link_target); ?>"><?= esc_html($link_title); ?> <i class="fa-regular fa-arrow-right"></i></a>
+                <?php endif; ?>
             </div>
         </div>
         <div class="content">

5781b7b55ed3da3f92a56bcd2b3a9d295c6e00bd
kvv1980 <ves1982@gmail.com>
2025-02-10 14:36:05 +0200

    add page

--- html/js/script.js
+++ html/js/script.js
@@ -18,4 +18,45 @@ jQuery(document).ready(function ($) {
     $(this).closest('li').toggleClass('is-active');
   })
 
+  //+/-
+  $(".btn-count-plus").click(function () {
+    var e = $(this).parent().find("input");
+    return e.val(parseInt(e.val()) + 1), e.change(), !1
+  }), $(".btn-count-minus").click(function () {
+    var e = $(this).parent().find("input"), t = parseInt(e.val()) - 1;
+    return t = t < 1 ? 1 : t, e.val(t), e.change(), !1
+  })
+
+  //file
+  $('input:file').change(function(){
+    $this = $(this);
+    $name = $this.val().replace('C:\\fakepath\\', '');
+    $(this).closest('.input-wrap-file').find('.file-select').text($name).show();
+    $(this).closest('.input-wrap-file').find('.h5').hide();
+    $(this).closest('.input-wrap-file').find('.h6').hide();
+    $(this).closest('.input-wrap-file').find('.img').hide();
+    $(this).closest('.input-wrap-file').find('.delete-file').show();
+  });
+
+  //clear input
+  $(document).on('click', '.delete-file', function (e){
+    e.preventDefault();
+    $(this).siblings('input').val('');
+    $(this).closest('.input-wrap-file').find('.file-select').text('').hide();
+    $(this).closest('.input-wrap-file').find('.h5').show();
+    $(this).closest('.input-wrap-file').find('.h6').show();
+    $(this).closest('.input-wrap-file').find('.img').show();
+    $(this).closest('.input-wrap-file').find('.delete-file').hide();
+  });
+
+
+  //mask
+  $('.cart-number').mask("0000 0000 0000 0000", {placeholder: "1234 1234 1234 1234"});
+  $('.cart-date').mask("00/00", {placeholder: "MM/AA"});
+  $('.cart-cvc').mask("000", {placeholder: "123"});
+
+  $(".fancybox").fancybox({
+    touch: false,
+    autoFocus: false,
+  });
 });

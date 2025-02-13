d7cbfc8067edd72c08b7a8bf138dc7d528ecc0fc
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2025-02-13 20:21:02 +0100

    offers

--- wp-theme/opalisa/templates/guttenberg/offers.php
+++ wp-theme/opalisa/templates/guttenberg/offers.php
@@ -24,77 +24,48 @@ $offers = get_field('offers');
                 <?php endif; ?>
             </div>
         </div>
-        <div class="content">
-            <div class="item">
-                <figure>
-                    <p class="label">offre spéciale</p>
-                    <a href="#">
-                        <img src="<?= get_template_directory_uri();?>/img/img-1-1.jpg" alt="">
-                    </a>
-                </figure>
-                <div class="text">
-                    <h6><a href="#">Mexico</a></h6>
-                    <ul>
-                        <li><img src="<?= get_template_directory_uri();?>/img/icon-2-1.svg" alt="">Vacances d’été</li>
-                        <li><img src="<?= get_template_directory_uri();?>/img/icon-2-2.svg" alt="">Classe économique</li>
-                    </ul>
-                    <div class="btn-wrap">
-                        <a href="#" class="btn-arrow">À partir de 490€ <i class="fa-solid fa-arrow-right-long"></i></a>
+        <?php if( $offers ): ?>
+            <div class="content">
+                <?php foreach( $offers as $post): setup_postdata($post);
+                    $id = $post->ID;
+                    $img = get_the_post_thumbnail_url($id);
+                    $product = wc_get_product( $id );
+
+                    $vacation = $product->get_attribute( 'vacation' );
+                    $class = $product->get_attribute( 'class' );
+
+                    if ( $product->is_on_sale() ) {
+                        $price = wc_price( $product->get_sale_price() );
+                    } else {
+                        $price =  wc_price( $product->get_price() );
+                    }?>
+
+                    <div class="item">
+                        <figure>
+                            <p class="label">offre spéciale</p>
+                            <a href="#" class="add-to-cart-btn" data-product_id="<?= $id;?>">
+                                <img src="<?= $img;?>" alt="">
+                            </a>
+                        </figure>
+                        <div class="text">
+                            <h6><a href="#" class="add-to-cart-btn" data-product_id="<?= $id;?>"><?= get_the_title($id);?></a></h6>
+                            <ul>
+                                <?php if ( !empty( $vacation ) ) {
+                                    echo '<li><img src="'. get_template_directory_uri().'/img/icon-2-1.svg" alt="">' .  esc_html( $vacation ) . '</li>';
+                                }
+
+                                if (!empty($class)) {
+                                    echo '<li><img src="' . get_template_directory_uri() . '/img/icon-2-2.svg" alt="">' . esc_html($class) . '</li>';
+                                }?>
+                            </ul>
+                            <div class="btn-wrap">
+                                <a href="#" class="add-to-cart-btn btn-arrow" data-product_id="<?= $id;?>"><?= __('À partir de', 'opalisa');?> <?= ' '.$price;?> <i class="fa-solid fa-arrow-right-long"></i></a>
+                            </div>
+                        </div>
                     </div>
-                </div>
+
+                <?php endforeach; wp_reset_postdata();?>
             </div>
-            <div class="item">
-                <figure>
-                    <p class="label">offre spéciale</p>
-                    <a href="#">
-                        <img src="<?= get_template_directory_uri();?>/img/img-1-2.jpg" alt="">
-                    </a>
-                </figure>
-                <div class="text">
-                    <h6><a href="#">Rio</a></h6>
-                    <ul>
-                        <li><img src="<?= get_template_directory_uri();?>/img/icon-2-1.svg" alt="">Vacances de la Toussaint</li>
-                        <li><img src="<?= get_template_directory_uri();?>/img/icon-2-2.svg" alt="">Classe économique</li>
-                    </ul>
-                    <div class="btn-wrap">
-                        <a href="#" class="btn-arrow">À partir de 760€ <i class="fa-solid fa-arrow-right-long"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="item">
-                <figure>
-                    <a href="#">
-                        <img src="<?= get_template_directory_uri();?>/img/img-1-3.jpg" alt="">
-                    </a>
-                </figure>
-                <div class="text">
-                    <h6><a href="#">Porto</a></h6>
-                    <ul>
-                        <li><img src="<?= get_template_directory_uri();?>/img/icon-2-1.svg" alt="">Vacances de Pâcques</li>
-                        <li><img src="<?= get_template_directory_uri();?>/img/icon-2-2.svg" alt="">Classe économique</li>
-                    </ul>
-                    <div class="btn-wrap">
-                        <a href="#" class="btn-arrow">À partir de 320€ <i class="fa-solid fa-arrow-right-long"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="item">
-                <figure>
-                    <a href="#">
-                        <img src="<?= get_template_directory_uri();?>/img/img-1-4.jpg" alt="">
-                    </a>
-                </figure>
-                <div class="text">
-                    <h6><a href="#">Le Caire</a></h6>
-                    <ul>
-                        <li><img src="<?= get_template_directory_uri();?>/img/icon-2-1.svg" alt="">Vacances d’hiver</li>
-                        <li><img src="<?= get_template_directory_uri();?>/img/icon-2-2.svg" alt="">Classe économique</li>
-                    </ul>
-                    <div class="btn-wrap">
-                        <a href="#" class="btn-arrow">À partir de 870€ <i class="fa-solid fa-arrow-right-long"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php endif;?>
     </div>
 </section>

cfaab82d7721ad82ec6abc7edbcad7ecb74c3d3d
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2025-02-14 13:47:28 +0100

    Update home_banner.php

--- wp-theme/opalisa/templates/guttenberg/home_banner.php
+++ wp-theme/opalisa/templates/guttenberg/home_banner.php
@@ -15,7 +15,7 @@ $image = get_field('image');
     <div class="content-width">
         <div class="title">
             <div class="left">
-                <?php if($title){ ?>}
+                <?php if($title){ ?>
                     <h1><?= $title;?></h1>
                 <?php } ?>
             </div>
@@ -28,24 +28,36 @@ $image = get_field('image');
         <div class="form-wrap">
             <form action="<?php echo esc_url(home_url('/shop/')); ?>" method="GET" class="form-banner">
                 <div class="input-wrap input-wrap-1">
-                    <label for="origin">Origine</label>
-                    <input type="text" id="origin" name="origin" placeholder="D’où partez vous ?" value="<?php echo esc_attr($_GET['origin'] ?? ''); ?>">
+                    <label for="origin"><?= __('Origine', 'opalisa');?></label>
+                    <input type="text" id="origin" name="origin" placeholder="<?= __('D’où partez vous ?', 'opalisa');?>" value="<?php echo esc_attr($_GET['origin'] ?? ''); ?>">
                 </div>
                 <div class="input-wrap input-wrap-2">
-                    <label for="destination">Destination</label>
-                    <input type="text" id="destination" name="destination" placeholder="Où allez vous ?" value="<?php echo esc_attr($_GET['destination'] ?? ''); ?>">
+                    <label for="destination"><?= __('Destination', 'opalisa');?></label>
+                    <input type="text" id="destination" name="destination" placeholder="<?= __('Où allez vous ?', 'opalisa');?>" value="<?php echo esc_attr($_GET['destination'] ?? ''); ?>">
                 </div>
                 <div class="input-wrap select-block input-wrap-3">
-                    <label for="select-1">Voyage durant les vacances scolaires ?</label>
-                    <select id="select-1" name="holiday">
-                        <option value="" disabled selected>Sélectionnez la période scolaire</option>
-                        <option value="1" <?php selected($_GET['holiday'] ?? '', '1'); ?>>Select 1</option>
-                        <option value="2" <?php selected($_GET['holiday'] ?? '', '2'); ?>>Select 2</option>
-                        <option value="3" <?php selected($_GET['holiday'] ?? '', '3'); ?>>Select 3</option>
-                    </select>
+                    <label for="select-1"><?= __('Voyage durant les vacances scolaires ?', 'opalisa');?></label>
+                    <?php
+                    $vacations = get_terms([
+                        'taxonomy'   => 'pa_vacation',
+                        'hide_empty' => false,
+                    ]);
+
+                    if (!empty($vacations) && !is_wp_error($vacations)): ?>
+                        <select id="select-1" name="holiday">
+                            <option value="" disabled selected><?= __('Sélectionnez la période scolaire', 'opalisa'); ?></option>
+                            <?php foreach ($vacations as $vacation): ?>
+                                <option value="<?= esc_attr($vacation->term_id); ?>"
+                                    <?php selected($_GET['holiday'] ?? '', $vacation->term_id); ?>>
+                                    <?= esc_html($vacation->name); ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                    <?php endif; ?>
+
                 </div>
                 <div class="input-wrap select-block input-wrap-4">
-                    <label for="select-2">Passager(s)</label>
+                    <label for="select-2"><?= __('Passager(s)', 'opalisa');?></label>
                     <select id="select-2" name="passengers">
                         <option value="1" <?php selected($_GET['passengers'] ?? '', '1'); ?>>1</option>
                         <option value="2" <?php selected($_GET['passengers'] ?? '', '2'); ?>>2</option>
@@ -53,15 +65,29 @@ $image = get_field('image');
                     </select>
                 </div>
                 <div class="bottom">
-                    <h6>Filtres :</h6>
-                    <ul>
-                        <li><label for="type_1"><input type="checkbox" name="class[]" id="type_1" value="first" <?php checked(in_array('first', $_GET['class'] ?? [])); ?>>First class</label></li>
-                        <li><label for="type_2"><input type="checkbox" name="class[]" id="type_2" value="business" <?php checked(in_array('business', $_GET['class'] ?? [])); ?>>Business class</label></li>
-                        <li><label for="type_3"><input type="checkbox" name="class[]" id="type_3" value="economy" <?php checked(in_array('economy', $_GET['class'] ?? [])); ?>>Classe économique</label></li>
-                    </ul>
+                    <h6><?= __('Filtres :', 'opalisa');?></h6>
+                    <?php
+                    $classes = get_terms([
+                        'taxonomy'   => 'pa_class',
+                        'hide_empty' => false,
+                    ]);
+
+                    if (!empty($classes) && !is_wp_error($classes)): ?>
+                        <ul>
+                            <?php foreach ($classes as $class): ?>
+                                <li>
+                                    <input type="checkbox" name="class[]" id="type_<?= esc_attr($class->term_id); ?>" value="<?= esc_attr($class->term_id); ?>" <?php checked(in_array($class->term_id, $_GET['class'] ?? [])); ?>>
+                                    <label for="type_<?= esc_attr($class->term_id); ?>">
+                                        <?= esc_html($class->name); ?>
+                                    </label>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endif; ?>
+
                 </div>
                 <div class="input-wrap-submit">
-                    <button type="submit" class="btn-arrow">Rechercher <i class="fa-solid fa-arrow-right-long"></i></button>
+                    <button type="submit" class="btn-arrow"><?= __('Rechercher', 'opalisa');?> <i class="fa-solid fa-arrow-right-long"></i></button>
                 </div>
             </form>
 

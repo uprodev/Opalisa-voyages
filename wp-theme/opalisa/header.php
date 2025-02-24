4e383bdd0277c1486da30d0e5c19e33c9915fee9
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2025-02-24 21:25:35 +0100

    Update header.php

--- wp-theme/opalisa/header.php
+++ wp-theme/opalisa/header.php
@@ -26,10 +26,9 @@
                 </a>
             </div>
             <div class="right">
-                <a href="<?= wc_get_cart_url();?>">
+                <a href="<?= wc_get_checkout_url();?>">
                     <img src="<?= get_template_directory_uri();?>/img/icon-1.svg" alt="cart">
-                    <span class="count-product"><?= WC()->cart->get_cart_contents_count()==0?'':WC()
-                            ->cart->get_cart_contents_count();
+                    <span class="count-product"><?= WC()->cart->get_cart_contents_count()==0?'':WC()->cart->get_cart_contents_count();
                     ?></span>
                 </a>
             </div>

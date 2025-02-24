603dc5f662683b6c738db91fa135dc4c8d8ae9e8
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2025-02-24 21:13:35 +0100

    Update content-product.php

--- wp-theme/opalisa/woocommerce/content-product.php
+++ wp-theme/opalisa/woocommerce/content-product.php
@@ -25,87 +25,138 @@ if ( ! is_a( $product, WC_Product::class ) || ! $product->is_visible() ) {
 }
 $id = $product->get_id();
 
+$class = $product->get_attribute( 'class' );
+
 $depart = get_field('ticket_from', $id);
 $arret = get_field('ticket_to', $id);
 $date_departure = get_field('date_departure', $id);
 $date_back = get_field('date_back', $id);
 $company = get_field('company', $id);
+$numero_de_vol_retour = get_field('numero_de_vol_retour', $id);
+$numero_de_vol = get_field('numero_de_vol', $id);
+$bagage = get_field('bagage', $id);
+$escales = get_field('escales_fly', $id);
+$escales2 = get_field('escales_fly_retour', $id);
+$time = get_field('time_departure_arrival', $id);
+$time_back = get_field('time_departure_arrival_back', $id);
+$duree_vol_aller = get_field('duree_vol_aller', $id);
+$timer = get_field('time_departure_arrival_retour', $id);
+$time_backr = get_field('time_departure_arrival_back_retour', $id);
+$duree_vol_retour = get_field('duree_vol_retour', $id);
+
 ?>
 <div class="item-content">
     <div class="left-item">
-        <h6 class="name">ALLER <?php if($company):?><img src="<?= $company['url'];?>" alt="<?= $company['alt'];?>"><?php endif;?> AF842</h6>
+        <h6 class="name"><?= __('ALLER', 'opalisa');?> <?php if($company):?><img src="<?= $company['url'];?>" alt="<?= $company['alt'];
+        ?>"><?php endif;?> <?= $numero_de_vol;?></h6>
         <div class="fly-wrap">
             <div class="city-block-1 city-block">
                 <h6 class="city"><?= $depart;?></h6>
                 <p><?= $date_departure;?></p>
-                <p>8h20</p>
+                <p><?= $time;?></p>
             </div>
             <div class="fly">
                 <div class="line"></div>
                 <div class="fly-content">
-                    <p>09h17</p>
+                    <p><?= $duree_vol_aller;?></p>
                     <figure>
                         <img src="<?= get_template_directory_uri();?>/img/icon-4.svg" alt="">
                     </figure>
-                    <p>2 escales</p>
-                    <span class="tooltip">
-												<i class="fa-solid fa-circle-info"></i>
-												<span class="tooltip-text">
-												<span class="text">Durée de l’escale à Paris : 1h40</span>
-												<span class="text">Durée de l’escale à Madrid : 2h10</span>
-											</span>
-											</span>
+                    <p><?= $escales?count($escales). __(' escales', 'opalisa'):__('Direct', 'opalisa');?></p>
+                    <?php if($escales):?>
+                        <span class="tooltip">
+                            <i class="fa-solid fa-circle-info"></i>
+                            <span class="tooltip-text">
+                                <?php foreach ($escales as $esl):?>
+                                    <span class="text"><?= $esl['escale'];?></span>
+                                <?php endforeach;?>
+                            </span>
+                        </span>
+                    <?php endif;?>
                 </div>
             </div>
             <div class="city-block-2 city-block">
                 <h6 class="city"><?= $arret;?></h6>
-                <p><?= $date_back;?></p>
-                <p>17h37</p>
+                <p><?= $date_departure;?></p>
+                <p><?= $time_back;?></p>
             </div>
         </div>
         <div class="full-line"></div>
-        <h6 class="name">retour <?php if($company):?><img src="<?= $company['url'];?>" alt="<?= $company['alt'];?>"><?php endif;?> AF738</h6>
+        <h6 class="name"><?= __('retour', 'opalisa');?> <?php if($company):?><img src="<?= $company['url'];?>" alt="<?= $company['alt'];
+        ?>"><?php endif;?> <?= $numero_de_vol_retour;?></h6>
         <div class="fly-wrap">
             <div class="city-block-1 city-block">
                 <h6 class="city"><?= $arret;?></h6>
-                <p>28 janvier 2024</p>
-                <p>9h14</p>
+                <p><?= $date_back;?></p>
+                <p><?= $time_backr;?></p>
             </div>
             <div class="fly">
                 <div class="line"></div>
                 <div class="fly-content">
-                    <p>08h34</p>
+                    <p><?= $duree_vol_retour;?></p>
                     <figure>
                         <img src="<?= get_template_directory_uri();?>/img/icon-4.svg" alt="">
                     </figure>
-                    <p>Direct</p>
+                    <p><?= $escales2?count($escales2). __(' escales', 'opalisa'):__('Direct', 'opalisa');?></p>
+                    <?php if($escales2):?>
+                        <span class="tooltip">
+                            <i class="fa-solid fa-circle-info"></i>
+                            <span class="tooltip-text">
+                                <?php foreach ($escales2 as $esl):?>
+                                    <span class="text"><?= $esl['escale'];?></span>
+                                <?php endforeach;?>
+                            </span>
+                        </span>
+                    <?php endif;?>
                 </div>
             </div>
             <div class="city-block-2 city-block">
                 <h6 class="city"><?= $depart;?></h6>
-                <p>28 janvier 2024</p>
-                <p>16h32</p>
+                <p><?= $date_back;?></p>
+                <p><?= $timer;?></p>
             </div>
         </div>
     </div>
     <div class="right-item">
-        <p class="img"><img src="<?= get_template_directory_uri();?>/img/icon-5-1.svg" alt="">Classe économique
-            <span class="tooltip">
-									<i class="fa-solid fa-circle-info"></i>
-									  <span class="tooltip-text">
-												<span class="text">Durée de l’escale à Paris : 1h40</span>
-												<span class="text">Durée de l’escale à Madrid : 2h10</span>
-									  </span>
-									</span>
-        </p>
-        <p class="img"><img src="<?= get_template_directory_uri();?>/img/icon-5-2.svg" alt="">Bagage en soute + cabine</p>
+        <?php if (!empty($class)):?>
+            <p class="img"><img src="<?= get_template_directory_uri();?>/img/icon-5-1.svg" alt=""><?= esc_html($class);?>
+                <?php if($escales):?>
+                    <span class="tooltip">
+                        <i class="fa-solid fa-circle-info"></i>
+                        <span class="tooltip-text">
+                            <?php foreach ($escales as $esl):?>
+                                <span class="text"><?= $esl['escale'];?></span>
+                            <?php endforeach;?>
+                        </span>
+                    </span>
+                <?php endif;?>
+            </p>
+        <?php endif;?>
+        <?php if($bagage):?>
+            <p class="img"><img src="<?= get_template_directory_uri();?>/img/icon-5-2.svg" alt="bagage"><?= $bagage; ?></p>
+        <?php endif;?>
 
         <div class="price">
             <?php woocommerce_template_loop_price();?>
         </div>
-        <p class="info">Plus que <b>12 billets</b></p>
+        <?php
+
+            $qtyp = $product->get_stock_quantity();
+
+            if( $qtyp > 12 ) {
+                $text_qty = __('Plus que', 'opalisa') . ' <b>12 '. __('billets', 'opalisa') . '</b>';
+            }elseif($qtyp == 1){
+                $text_qty = '<b>1 '. __('billet', 'opalisa') . '</b>';
+            }elseif($qtyp > 1 && $qtyp <= 12){
+                $text_qty = '<b>'. $qtyp .' '. __('billets', 'opalisa') . '</b>';
+            }else{
+                $text_qty = '';
+            }
+
+        ?>
+        <p class="info"><?= $text_qty;?></p>
         <div class="btn-wrap">
-            <a href="#" class="btn-arrow add-to-cart-btn" data-product_id="<?= $id;?>">Réserver<i class="fa-solid fa-arrow-right-long"></i></a>
+            <a href="#" class="btn-arrow add-to-cart-btn" data-product_id="<?= $id;?>"><?= __('Réserver', 'opalisa');?><i class="fa-solid fa-arrow-right-long"></i></a>
         </div>
     </div>
 </div>

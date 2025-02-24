53b2f41694a29e9393245198544e8ee7ce6e46f7
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2025-02-24 15:04:48 +0100

    shop

--- wp-theme/opalisa/woocommerce/archive-product.php
+++ wp-theme/opalisa/woocommerce/archive-product.php
@@ -19,6 +19,8 @@ defined( 'ABSPATH' ) || exit;
 
 get_header();
 
+woocommerce_output_all_notices();
+
 ?>
 
 <section class="product-banner">
@@ -34,39 +36,9 @@ get_header();
     <div class="content-width">
         <div class="aside">
             <div class="item item-1">
-                <form action="#" class="form-delivery">
-                    <div class="input-wrap">
-                        <label for="city1">Ville de départ</label>
-                        <input type="text" id="city1" name="city1" placeholder="D’où partez vous ?">
-                    </div>
-                    <div class="input-wrap">
-                        <label for="city2">Ville d’arrivée</label>
-                        <input type="text" id="city2" name="city2" placeholder="Où allez vous ?">
-                    </div>
-                    <p>Quelles vacances scolaires ?</p>
-                    <div class="input-wrap-check">
-                        <div class="item-wrap">
-                            <input type="checkbox" id="check1" name="check1">
-                            <label for="check1">Vacances de la Toussaint</label>
-                        </div>
-                        <div class="item-wrap">
-                            <input type="checkbox" id="check2" name="check1">
-                            <label for="check2">Vacances de Noël</label>
-                        </div>
-                        <div class="item-wrap">
-                            <input type="checkbox" id="check3" name="check1">
-                            <label for="check3">Vacances d’hiver</label>
-                        </div>
-                        <div class="item-wrap">
-                            <input type="checkbox" id="check4" name="check1">
-                            <label for="check4">Vacances de printemps</label>
-                        </div>
-                        <div class="item-wrap">
-                            <input type="checkbox" id="check5" name="check1">
-                            <label for="check5">Vacances d’été</label>
-                        </div>
-                    </div>
-                </form>
+                <div class="form-delivery">
+                    <?= do_shortcode('[br_filters_group group_id=183]');?>
+                </div>
             </div>
             <div class="item item-2">
                 <h5>Autres dates ?</h5>
@@ -79,64 +51,36 @@ get_header();
         <div class="main">
             <div class="select-line">
                 <div class="left-select">
-                    <p><span>9</span> résultats sur 68</p>
+                    <?php woocommerce_result_count();?>
                 </div>
                 <div class="select">
-
-                    <div class="nice-select">
-                        <span class="current">Prix croissant</span>
-                        <ul class="list">
-                            <li data-value="0" class="option selected"><a href="#">Prix croissant</a></li>
-                            <li data-value="1" class="option"><a href="#">Prix croissant 1</a></li>
-                            <li data-value="2" class="option"><a href="#">Prix croissant 2</a></li>
-                            <li data-value="3" class="option"><a href="#">Prix croissant 3</a></li>
-                        </ul>
-                    </div>
+                    <?php woocommerce_catalog_ordering();?>
                 </div>
             </div>
             <div class="content-main">
                 <?php if ( woocommerce_product_loop() ) {
 
-	/**
-	 * Hook: woocommerce_before_shop_loop.
-	 *
-	 * @hooked woocommerce_output_all_notices - 10
-	 * @hooked woocommerce_result_count - 20
-	 * @hooked woocommerce_catalog_ordering - 30
-	 */
-	do_action( 'woocommerce_before_shop_loop' );
+                    if ( wc_get_loop_prop( 'total' ) ) {
+                        while ( have_posts() ) {
+                            the_post();
 
-	woocommerce_product_loop_start();
+                            /**
+                             * Hook: woocommerce_shop_loop.
+                             */
+                            do_action( 'woocommerce_shop_loop' );
 
-	if ( wc_get_loop_prop( 'total' ) ) {
-		while ( have_posts() ) {
-			the_post();
+                            wc_get_template_part( 'content', 'product' );
+                        }
+                    }
 
-			/**
-			 * Hook: woocommerce_shop_loop.
-			 */
-			do_action( 'woocommerce_shop_loop' );
-
-			wc_get_template_part( 'content', 'product' );
-		}
-	}
-
-	woocommerce_product_loop_end();
-
-	/**
-	 * Hook: woocommerce_after_shop_loop.
-	 *
-	 * @hooked woocommerce_pagination - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop' );
-} else {
-	/**
-	 * Hook: woocommerce_no_products_found.
-	 *
-	 * @hooked wc_no_products_found - 10
-	 */
-	do_action( 'woocommerce_no_products_found' );
-}?>
+                } else {
+                    /**
+                     * Hook: woocommerce_no_products_found.
+                     *
+                     * @hooked wc_no_products_found - 10
+                     */
+                    do_action( 'woocommerce_no_products_found' );
+                }?>
             </div>
         </div>
     </div>
--- wp-theme/opalisa/woocommerce/loop/result-count.php
+++ wp-theme/opalisa/woocommerce/loop/result-count.php
@@ -20,23 +20,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+$last = min( $total, $per_page * $current );
 ?>
-<p class="woocommerce-result-count" <?php echo ( empty( $orderedby ) || 1 === intval( $total ) ) ? '' : 'role="alert" aria-relevant="all" data-is-sorted-by="true"'; ?>>
-	<?php
-	// phpcs:disable WordPress.Security
-	if ( 1 === intval( $total ) ) {
-		_e( 'Showing the single result', 'woocommerce' );
-	} elseif ( $total <= $per_page || -1 === $per_page ) {
-		$orderedby_placeholder = empty( $orderedby ) ? '%2$s' : '<span class="screen-reader-text">%2$s</span>';
-		/* translators: 1: total results 2: sorted by */
-		printf( _n( 'Showing all %1$d result', 'Showing all %1$d results', $total, 'woocommerce' ) . $orderedby_placeholder, $total, esc_html( $orderedby ) );
-	} else {
-		$first                 = ( $per_page * $current ) - $per_page + 1;
-		$last                  = min( $total, $per_page * $current );
-		$orderedby_placeholder = empty( $orderedby ) ? '%4$s' : '<span class="screen-reader-text">%4$s</span>';
-		/* translators: 1: first result 2: last result 3: total results 4: sorted by */
-		printf( _nx( 'Showing %1$d&ndash;%2$d of %3$d result', 'Showing %1$d&ndash;%2$d of %3$d results', $total, 'with first and last result', 'woocommerce' ) . $orderedby_placeholder, $first, $last, $total, esc_html( $orderedby ) );
-	}
-	// phpcs:enable WordPress.Security
-	?>
-</p>
+<p class="woocommerce-result-count"><span><?php echo $last; ?></span> résultats sur <?php echo $total; ?></p>
